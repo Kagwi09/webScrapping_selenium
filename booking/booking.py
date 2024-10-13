9c5f12ee3d725b814ce54d167212ffbebb678bdb
Kagwi09 <kagwimwangi.m@gmail.com>
2024-10-13 20:57:01 +0300

    defined a method for changing the currency used to USD and another for closing unnecessary pop up windows

--- booking/booking.py
+++ booking/booking.py
@@ -1,5 +1,6 @@
 import booking.constants as const
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import os
 
 
@@ -12,6 +13,10 @@ class Booking(webdriver.Chrome):
         os.environ['PATH'] += r"C:\Users\MWANGI\Documents\Python Scripts\selenium\chromedriver-win64"
         super(Booking, self).__init__()
 
+        # add an implicit wait clause
+        self.implicitly_wait(15)
+        self.maximize_window()
+
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.teardown:
             self.quit()
@@ -19,3 +24,28 @@ class Booking(webdriver.Chrome):
 
     def land_first_page(self):
         self.get(const.BASE_URL)
+
+    def close_popup(self):
+        close_button = self.find_element(By.CSS_SELECTOR,'button[aria-label="Dismiss sign-in info."]')
+        close_button.click()
+
+    def change_currency(self, currency=None):
+        # Find and click the currency picker button
+        currency_element = self.find_element(
+            By.CSS_SELECTOR, 'button[data-testid="footer-currency-picker-trigger-desktop"]'
+        )
+        currency_element.click()
+
+        # Use the *= operator to match the div that contains the class "CurrencyPicker_currency"
+        currency_selector = f'div[class*="CurrencyPicker_currency"]'
+
+        # Find all currency elements matching the selector
+        currency_elements = self.find_elements(By.CSS_SELECTOR, currency_selector)
+
+        # Iterate over the currency elements and click the one matching the specified currency (e.g., 'USD')
+        for element in currency_elements:
+            if currency in element.text:
+                element.click()  # Click the element that contains the specified currency
+                break
+
+
--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from booking.booking import Booking
 
-with Booking() as bot:
+with Booking(tear_down=True) as bot:
     bot.land_first_page()
-    # add tear_down=True if you want
-    # to close the browser after loading
+    bot.close_popup()
+    bot.change_currency(currency='USD')
+    input("Press Enter to close the browser...")
+#     # add tear_down=True if you want
+#     # to close the browser after loading
